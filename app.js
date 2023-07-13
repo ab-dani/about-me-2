d548ae04ceff0f107bb1a65501ad15b5831b4ee4
Antonio Brown <135006436+ab-dani@users.noreply.github.com>
2023-07-12 19:56:42 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (questionOneGuess === "y" || questionOneGuess === "yes") {
   alert("Sorry that's incorrect");
 }
 
-let questionTwoGuess = prompt(`Are you a forex trader ?`).toLpperCase();
+let questionTwoGuess = prompt(`Are you a forex trader ?`).toLowerCase();
 
 console.log(questionTwoGuess);
 
@@ -59,4 +59,54 @@ if (questionFiveGuess === "Y" || questionFiveGuess === `yes`) {
   alert("Great job! You are getting closer to being healthy");
 } else if (questionFiveGuess === "N" || questionFiveGuess === "no") {
   alert("It is ok you will get there. Keep trying! ");
+}
+
+let questionSixGuess = null;
+
+var correctAnswer = 12;
+var attempts = 4;
+while (questionSixGuess != correctAnswer) {
+  if (attempts === 0) {
+    alert("You're out of attempts. Correct answer is " + correctAnswer);
+    break;
+  }
+  questionSixGuess = parseInt(prompt(`Guess the number I am thinking?`));
+  if (questionSixGuess === correctAnswer) {
+    alert("You are correct! Great job!");
+  } else if (questionSixGuess > correctAnswer) {
+    alert("You're too high!");
+  } else if (questionSixGuess < correctAnswer) {
+    alert("You're too low!");
+  }
+  attempts--;
+}
+
+let array = [
+  "nashville",
+  "tampa",
+  "miami",
+  "new orleans",
+  "houston",
+  "los angelos",
+  "phoenix",
+  "seattle",
+];
+let questionSevenGuess = null;
+var attemptsCity = 6;
+questionsevenWhileloop: while (attemptsCity != 0) {
+  attemptsCity--
+  questionSevenGuess = prompt(
+    `Guess the city I am thinking about?`
+  ).toLowerCase();
+  for (let i = 0; i < array.length; i++) {
+    if (questionSevenGuess === array[i]) {
+      alert("You are correct! Good job!")
+      break questionsevenWhileloop
+    }
+  
+  }
+ alert("Nope You're wrong!");
+ if (attemptsCity = 0){
+  alert("You are out of attempts");
+ } 
 }
